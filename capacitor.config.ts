50331008bc294548d023182fcfe7fd0e24ba9ae2
marcosborg <m.borges.mail@gmail.com>
2024-03-12 23:56:10 +0000

    Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'gymspot',
+  appId: 'pt.gymspot.app',
+  appName: 'GymSpot',
   webDir: 'www/browser',
   server: {
     androidScheme: 'https'
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,8 +10,8 @@ export class ApiService {
     private http: HttpClient
   ) { }
 
-  url: string = 'http://127.0.0.1:8000/api/v2/';
-  protected_url: string = 'http://127.0.0.1:8000/api/v1/';
+  url: string = 'https://gymspot.pt/api/v2/';
+  protected_url: string = 'https://gymspot.pt/api/v1/';
 
   getSliders() {
     return this.http.get(this.url + 'sliders');
